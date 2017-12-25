fc87ab02bf1dd20d049850b65fdb83b488443bf1
YeFan <919024797@qq.com>
2017-12-25 16:08:06 +0800

    first
    Signed-off-by: YeFan <919024797@qq.com>

---
+++ module/date_time.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+""" a test module """
+
+__author__ = 'YF'
+
+from datetime import datetime
+
+dt = datetime(2015, 4, 19, 12, 20) # 用指定日期时间创建datetime
+print(dt)
+
+
