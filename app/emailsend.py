0fd515002f60f1132f59bb333ef35cb8e720085a
YeFan <919024797@qq.com>
2018-01-03 16:12:05 +0800

    SSL发送邮件
    Signed-off-by: YeFan <919024797@qq.com>

---
+++ app/emailsend.py
@@ -0,0 +1,25 @@
+from email.mime.text import MIMEText
+from email.header import Header
+import smtplib
+
+# 输入Email地址和口令:
+from_addr = 'yefan1_test@163.com'
+password = '1994421这个'
+# 输入收件人地址:
+to_addr = 'yefan_yf1@sina.com'
+# 输入SMTP服务器地址:
+smtp_server = 'smtp.163.com'
+subject = 'nihao'
+
+# 发件邮箱和收件邮箱需要真实地址，否则无法发送
+msg = MIMEText('hello, send by Python...', 'plain', 'utf-8')
+msg['Subject'] = Header(subject, 'utf-8')
+msg['From'] = 'yefan1_test@163.com'
+msg['To'] = 'yefan_yf1@sina.com'
+
+
+server = smtplib.SMTP_SSL(smtp_server, 587)  # SMTP协议默认端口是25
+server.set_debuglevel(1)
+server.login(from_addr, password)
+server.sendmail(from_addr, [to_addr], msg.as_string())
+server.quit()
